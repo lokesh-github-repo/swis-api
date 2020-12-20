7893e5d3de33da7c8b712754242036ec136a9dd1
lokesh-vishwas <lvishwas@astegic.com>
2020-12-20 15:34:24 +0530

    initial-commit

---
+++ swis-api/src/main/java/com/swis/SwisApiApplication.java
@@ -0,0 +1,13 @@
+package com.swis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwisApiApplication.class, args);
+	}
+
+}
---
+++ swis-api/src/test/java/com/swis/SwisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
